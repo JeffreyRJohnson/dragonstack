19c4573fd041e75c0d9f4988338d9dbe88fec92e
JeffreyRJohnson <jeffreyrjohnsonsemail@gmail.com>
2018-09-20 18:23:07 -0500

    added traits

---
+++ backend/app/generation/table.js
@@ -0,0 +1,23 @@
+const pool = require("../../databasePool");
+
+class GenerationTable {
+  static storeGeneration(generation) {
+    return new Promise((resolve, reject) => {
+      pool.query(
+        "INSERT INTO generation(expiration) VALUES($1) RETURNING id",
+        [generation.expiration],
+        (error, response) => {
+          if (error) return reject(error);
+
+          const generationId = response.rows[0].id;
+
+          resolve({ generationId });
+
+          return generationId;
+        }
+      );
+    });
+  }
+}
+
+module.exports = GenerationTable;
